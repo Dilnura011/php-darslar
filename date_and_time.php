758c0368e56a1a95692d0799a6fc4680a45afefb
Dilnura011 <nilufarotaboyeva60@gmail.com>
2025-11-15 15:34:27 +0500

    date and time

--- date_and_time.php
+++ date_and_time.php
@@ -12,5 +12,14 @@ echo date("d F Y")."<br>";
 echo time()."<br>";
 echo date("d.m.Y")."<br>";
 echo date("d.m.Y,",strtotime("2025-11-23"))."<br>";
-
+//Unix time
+echo "Unix time: ".time()."<br>";
+echo "Unix time: ".strtotime("2025-11-14")."<br>";
+// 15.11.2025
+echo strtotime("+7 days"). "<br>";
+echo date("d.m.Y H:i",strtotime("+7 days")). "<br>";
+echo date("Y.m.d",strtotime("last Monday")). "<br>";
+echo date("Y.m.d",strtotime("next Year")). "<br>";
+echo date("Y.m.d",strtotime("2024-11-14")). "<br>";
+echo date("d.m.Y H:i",strtotime("-10 days")). "<br>";
 ?>
